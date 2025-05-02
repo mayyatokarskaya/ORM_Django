33c672eb8a47eb655f1aca4b31cc4e829231bda3
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-02 23:07:15 +1000

    refactor urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("catalog.urls", namespace="catalog/")),
-]
-
-
-
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings  # Добавляем импорт settings
 from django.conf.urls.static import static  # Добавляем импорт static
 
