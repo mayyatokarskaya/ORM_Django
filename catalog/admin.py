c7fbc2f272bef8af05bc8a8881fa667588d787b5
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-02 23:17:00 +1000

    registration of models in the admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Настройки админки для категорий."""
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    ordering = ('id',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Настройки админки для продуктов."""
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+    list_editable = ('price',)
+
+
