ecd013eafb7c5214ef434cf442299f678f85af4b
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-04 20:47:57 +1000

    create controller "product_detail"

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
     return render(request, "home.html")
@@ -13,3 +15,9 @@ def contacts(request):
         message = request.POST.get("message")
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, "contacts.html")
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product}
+    return render(request, 'products/product_detail.html', context)
