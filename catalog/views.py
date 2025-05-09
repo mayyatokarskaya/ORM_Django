df3541cd4cbcad7feaf49c54efef5990da4675ad
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-09 12:50:22 +1000

    Controllers have been converted to CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,30 +1,28 @@
-from django.core.paginator import Paginator
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, View, TemplateView
 from django.shortcuts import render
-
-from catalog.models import Product
-
-
-def home(request):
-    product_list = Product.objects.all()
-    paginator = Paginator(product_list, 4)
-
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(request, "home.html", {"page_obj": page_obj})
+from django.http import HttpResponse
+from .models import Product
+from django.core.paginator import Paginator
 
 
-def contacts(request):
-    if request.method == "POST":
-        # Получение данных из формы
+class HomePageView(ListView):
+    model = Product
+    template_name = "home.html"
+    context_object_name = "page_obj"
+    paginate_by = 4
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+    context_object_name = "product"
+
+
+class ContactsView(View):
+    def get(self, request):
+        return render(request, "contacts.html")
+
+    def post(self, request):
         name = request.POST.get("name")
         message = request.POST.get("message")
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, "contacts.html")
-
-
-def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {"product": product}
-    return render(request, "product_detail.html", context)
