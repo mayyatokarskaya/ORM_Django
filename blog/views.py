7432f570ce1e0820f8503d289ad489f155964a91
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-09 15:14:56 +1000

    blog link

--- blog/views.py
+++ blog/views.py
@@ -10,11 +10,22 @@ class BlogListView(ListView):
     template_name = "blog/blog_list.html"
     context_object_name = "posts"
 
+    def get_queryset(self):
+        # Фильтруем только опубликованные записи
+        return BlogPost.objects.filter(is_published=True)
+
 
 class BlogDetailView(DetailView):
     model = BlogPost
     template_name = "blog/blog_detail.html"
     context_object_name = "post"
+
+    def get_object(self, queryset=None):
+        # Получаем объект, увеличиваем счетчик просмотров и сохраняем
+        obj = super().get_object(queryset)
+        obj.view_count += 1
+        obj.save()
+        return obj
 
 
 class BlogCreateView(CreateView):
@@ -28,7 +39,13 @@ class BlogUpdateView(UpdateView):
     model = BlogPost
     template_name = "blog/blog_form.html"
     fields = ["title", "content", "preview_image", "is_published"]
-    success_url = reverse_lazy("blog:blog_list")
+    # success_url = reverse_lazy("blog:blog_list")
+
+    def get_success_url(self):
+        # Перенаправляем на страницу редактируемой статьи
+        return reverse_lazy("blog:blog_detail", kwargs={"pk": self.object.pk})
+
+
 
 
 class BlogDeleteView(DeleteView):
