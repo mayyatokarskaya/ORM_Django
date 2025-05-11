a70fcb7a9e0fbb8b1640a505bcac0d286a214a0c
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-11 13:51:16 +1000

    added link to form

--- catalog/views.py
+++ catalog/views.py
@@ -27,20 +27,20 @@ class ProductDetailView(DetailView):
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
-    template_name = "catalog/product_form.html"
+    template_name = "product_form.html"
     success_url = reverse_lazy("catalog:home")
 
 
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
-    template_name = "catalog/product_form.html"
+    template_name = "product_form.html"
     success_url = reverse_lazy("catalog:home")
 
 
 class ProductDeleteView(DeleteView):
     model = Product
-    template_name = "catalog/product_confirm_delete.html"
+    template_name = "product_confirm_delete.html"
     success_url = reverse_lazy("catalog:home")
 
 
