3096339f5f5654d0257cf19f7c31c756b737f3a9
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-11 13:36:47 +1000

    update catalog/views.py - add form_class

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,14 @@
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView, TemplateView, View
+from django.urls import reverse_lazy
+from django.views.generic import (
+    DetailView, ListView, TemplateView, View,
+    CreateView, UpdateView, DeleteView
+)
 
 from .models import Product
+from .forms import ProductForm  # подключаем форму
 
 
 class HomePageView(ListView):
@@ -17,6 +22,26 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = "product_detail.html"
     context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy("catalog:home")
 
 
 class ContactsView(View):
