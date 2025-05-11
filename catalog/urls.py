93a60758c15441cbd89e9bab7ee0f645956263bb
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-11 13:39:41 +1000

    added routes for form work

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import ContactsView, HomePageView, ProductDetailView
+from .views import (
+    ContactsView,
+    HomePageView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+)
 
 app_name = "catalog"
 
@@ -8,4 +15,8 @@ urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
     path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/create/", ProductCreateView.as_view(), name="product_create"),
+    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]
+
