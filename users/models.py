79018f1a31db8640aec96fa1e26f0dd5677c650d
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-14 20:44:21 +1000

    create users/models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    username = None  # Отключаем username, если не нужен
+    email = models.EmailField(unique=True, verbose_name="Email")
+
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name="Аватар")
+    phone_number = models.CharField(max_length=20, blank=True, null=True, verbose_name="Телефон")
+    country = models.CharField(max_length=100, blank=True, null=True, verbose_name="Страна")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # Не запрашивать другие поля при создании суперпользователя
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
