c20284837cfaf955fc95adf1b25723d55301d13f
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-14 21:49:47 +1000

    create views.py in users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.core.mail import send_mail
+from .models import CustomUser
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class RegisterView(CreateView):
+    model = CustomUser
+    form_class = CustomUserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # Отправка письма
+        send_mail(
+            'Добро пожаловать!',
+            'Спасибо за регистрацию на нашем сайте.',
+            'from@example.com',  # Укажи свой адрес отправителя
+            [self.object.email],
+            fail_silently=False,
+        )
+
+        return response
