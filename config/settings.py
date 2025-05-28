1765f51f95f4bc8677a0420682a0957fcb45ca0e
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-28 20:12:16 +1000

    setting radis

--- config/settings.py
+++ config/settings.py
@@ -117,3 +117,11 @@ EMAIL_USE_SSL = True
 EMAIL_HOST_USER = "test.test.Django@yandex.com"
 EMAIL_HOST_PASSWORD = "hyofjzzgzpcyrktl"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
